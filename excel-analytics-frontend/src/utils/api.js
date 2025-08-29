8ae3d6411c4dda50eb4731a20774742a00e995c2
ksuvii21 <k21ritikasuvi2106@gmail.com>
2025-08-29 16:42:17 +0530

    Setup frontend deploy to gh-pages

--- excel-analytics-frontend/src/utils/api.js
+++ excel-analytics-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Use relative path for API requests in development
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://excel-analytics-zo51.onrender.com/api';
 const API = axios.create({ baseURL: API_URL });
 
 // Set up Axios instance
--- excel-analytics-frontend/vite.config.js
+++ excel-analytics-frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Excel-Analytics/",
   plugins: [react()],
   server: {
     proxy: {
